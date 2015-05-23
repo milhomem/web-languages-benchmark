7d0dbd0530d1e0cdc3cc0af0bf870c3b0d380be9
Marcelo Milhomem <marcelo.milhomem@easytaxi.com.br>
2015-05-23 14:38:04 -0300

    PHP server setup
    Add setup instructions for running PHP-FPM server
    Add PHP script to serve requests

---
+++ php/README.md
@@ -0,0 +1,37 @@
+# PHP server
+
+## Dependencies
+* PHP v5.5+
+* PHP-FPM
+* NoSQL Database MongoDB 2.4+
+* Nginx 1.4.6+
+* Silex Framework
+
+## Installing and configuring
+```bash
+curl -sS https://getcomposer.org/installer | php
+composer install
+ln -s /path/to/nginx.conf /etc/nginx/sites-enabled/benchmark.conf
+/etc/init.d/nginx restart
+```
+
+## Start the server
+```bash
+ln -s /path/to/php-fpm-pool.conf /etc/php5/fpm/pool.d/benchmark.conf
+/etc/init.d/php5-fpm restart
+```
+
+## Testing
+```bash
+curl 192.168.33.10:8080/taxi-position --request PUT -d '{"lat": -19.432608, "long": -99.133208}' -H 'Accept: application/json' -H 'Content-type: application/json'
+```
+
+## Stop the server
+```bash
+/etc/init.d/php5-fpm stop
+```
+
+## References
+* http://www.if-not-true-then-false.com/2011/nginx-and-php-fpm-configuration-and-optimizing-tips-and-tricks/
+* https://getcomposer.org/download/
+* http://silex.sensiolabs.org/doc/intro.html
---
+++ php/index.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+  
+$app = new Silex\Application();
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . "/../config.yml"));
+
+$app['db'] = $app->share(function ($app) {
+    $host = $app['mongodb']['host'] . ':' . $app['mongodb']['port'];
+    $user = $app['mongodb']['username'];
+    $password = $app['mongodb']['password'];
+    $database = $app['mongodb']['database'];
+
+    $auth = $user && $password ? "{$user}:{$password}@" : '';
+    $connectionString = "mongodb://{$auth}{$host}";
+    $connection = new \MongoClient($connectionString);
+
+    return $connection->$database;
+});
+
+$app->before(
+    function (Request $request) use ($app) {
+        $requestPayload = $request->getContent();
+        $requestPayloadData = json_decode($requestPayload, true);
+
+        if (!empty($requestPayloadData)) {
+            $request->request->replace($requestPayloadData);
+        }
+    }
+);
+
+$app->put('taxi-position', function(Request $request) use ($app) {
+    $jsonRequest = $request->request->getIterator();
+
+    $collection = $app['db']->selectCollection($app['mongodb']['collection']);
+
+    $criteria = ['_id' => new \MongoId('556018800640fd52df330d31')];
+    $positionRecord = ['lat' => $jsonRequest['lat'], 'long' => $jsonRequest['long']];
+    $flags = ['upsert' => 1];
+    $collection->update($criteria, $positionRecord, $flags);
+
+	return new Response(null, 204);
+});
+
+$app->run();
