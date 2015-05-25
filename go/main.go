d046c28d2d92b852fdc2c906dd2937cb8306c7ed
Leandro Repolho <leandrorepolho@gmail.com>
2015-05-25 12:06:59 -0300

    GO Lang server setup
    Add setup instructions for running GO Lang
    Add GO Lang script to serve requests

---
+++ go/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+    "io/ioutil"
+    "path/filepath"
+    "fmt"
+    "github.com/gnagel/go-geohash/ggeohash"
+    "github.com/smallfish/simpleyaml"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+    "github.com/hoisie/web"
+)
+
+type TaxiPosition struct {
+    Lat float64 `json:"lat"`
+    Long float64 `json:"long"`
+    Geohash string
+}
+
+func getConfiguration(configPath string) (string, int, string, string) {
+    file, _ := filepath.Abs(configPath)
+    configYaml, _ := ioutil.ReadFile(file)
+    config, _ := simpleyaml.NewYaml(configYaml)
+
+    dbHost, _ := config.Get("mongodb").Get("host").String();
+    dbPort, _ := config.Get("mongodb").Get("port").Int();
+    dbName, _ := config.Get("mongodb").Get("database").String();
+    dbCollection, _ := config.Get("mongodb").Get("collection").String();
+
+    return dbHost, dbPort, dbName, dbCollection
+}
+
+func putTaxiPosition(ctx *web.Context) string {
+    dbHost, dbPort, dbName, dbCollection := getConfiguration("../config.yml")
+
+    body, _ := ioutil.ReadAll(ctx.Request.Body)
+    taxiPosition := &TaxiPosition{}
+    json.Unmarshal(body, &taxiPosition)
+
+    taxiPosition.Geohash = ggeohash.Encode(taxiPosition.Lat, taxiPosition.Long, 9)
+
+    session, err := mgo.Dial(fmt.Sprintf("mongodb://%v:%v", dbHost, dbPort))
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer session.Close()
+    c := session.DB(dbName).C(dbCollection)
+
+    info, err := c.UpsertId(bson.ObjectIdHex("556018800640fd52df330d31"), taxiPosition)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if info != nil {
+        log.Print(info)
+    }
+
+    ctx.WriteHeader(204)
+
+    return ""
+}
+
+func main() {
+    web.Put("/taxi-position", putTaxiPosition)
+    web.Run("0.0.0.0:9000")
+}
