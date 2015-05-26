56ca870d669f1bd0c8bdc600fb8c79b6a43dc878
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2015-05-26 14:16:11 -0300

    Include strerr and out

--- ruby/unicorn.rb
+++ ruby/unicorn.rb
@@ -9,5 +9,11 @@ timeout 30
 
 # Specify path to socket unicorn listens to,
 # we will use this in our nginx.conf later
-listen "0.0.0.0:9000", :backlog => 64
+listen '0.0.0.0:9000', :backlog => 64
 
+# Set process id path
+pid ENV.fetch('unicorn_pid', '/var/run/unicorn.pid')
+
+# Set log file paths
+stderr_path ENV.fetch('unicorn_stderr', '/var/log/unicorn.stderr.log')
+stdout_path ENV.fetch('unicorn_stdout', '/var/log/unicorn.stdout.log')
