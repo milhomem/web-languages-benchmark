4268dacf3c0ab671a0e3f099c6542680394d7fc1
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2015-05-26 15:46:06 -0300

    Separate flask

--- python/flask/README.md
+++ python/flask/README.md
@@ -9,24 +9,25 @@
 
 ## Installing and configuring
 ```bash
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python2.7
 apt-get install build-essential python python-dev
 pip install virtualenv
 virtualenv .
 source bin/activate
 pip install -r requirements.txt
-ln -s /path/to/nginx.conf /etc/nginx/sites-enabled/benchmark.conf
+ln -s $PWD/nginx.conf /etc/nginx/sites-enabled/python.conf
 /etc/init.d/nginx restart
 ```
 
 ## Start the server
 ```bash
-uwsgi --socket :9000 --wsgi-file index.py --callable app --master --processes 8 --threads 1 1>/var/log/uwsgi.log 2>&1 &
+uwsgi --socket :9004 --wsgi-file index.py --callable app --master --processes 8 --threads 1 1>/var/log/uwsgi.log 2>&1 &
 echo $! > /var/run/uwsgi.pid
 ```
 
 ## Testing
 ```bash
-curl 192.168.33.10:8080/taxi-position --request PUT -d '{"lat": -19.432608, "long": -99.133208}' -H 'Accept: application/json' -H 'Content-type: application/json'
+curl localhost:8084/taxi-position --request PUT -d '{"lat": -19.432608, "long": -99.133208}' -H 'Accept: application/json' -H 'Content-type: application/json'
 ```
 
 ## Stop the server
--- python/flask/index.py
+++ python/flask/index.py
@@ -5,36 +5,33 @@ from mongoengine import *
 from bson.objectid import ObjectId
 import os, yaml
 
-configFile = os.path.join(os.getcwd(), '../..', 'config.yml')
+configFile = os.path.join(os.getcwd(), '..', 'config.yml')
 with open(configFile, 'r') as ymlfile:
-	config = yaml.load(ymlfile)
+        config = yaml.load(ymlfile)
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {
-	'DB': config['mongodb']['database'],
-	'HOST': config['mongodb']['host'],
-	'PORT': config['mongodb']['port'],
-	'USERNAME': config['mongodb']['username'],
-	'PASSWORD': config['mongodb']['password']
+        'DB': config['mongodb']['database'],
+        'HOST': config['mongodb']['host'],
+        'PORT': config['mongodb']['port'],
+        'USERNAME': config['mongodb']['username'],
+        'PASSWORD': config['mongodb']['password']
 }
 db = MongoEngine(app)
 
 class Position(DynamicDocument):
-	lat = FloatField(required=True)
-	long = FloatField(required=True)
-	meta = {'collection': config['mongodb']['collection']}
+        lat = FloatField(required=True)
+        long = FloatField(required=True)
+        meta = {'collection': config['mongodb']['collection']}
 
 @app.route("/taxi-position", methods=['PUT'])
 def position_update():
-	jsonRequest = request.json
+        jsonRequest = request.json
 
-	Position(
-		id=ObjectId('556018800640fd52df330d31'),
-		lat=jsonRequest['lat'],
-		long=jsonRequest['long']
-	).save()
+        Position(
+                id=ObjectId('556018800640fd52df330d31'),
+                lat=jsonRequest['lat'],
+                long=jsonRequest['long']
+        ).save()
 
-	return ('', 204)
-
-# if __name__ == "__main__":
-# 	app.run(host='0.0.0.0', port=config['proxy']['listen'])
+        return ('', 204)
